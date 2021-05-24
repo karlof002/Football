e3747d2bb8682407fc9b201d88992f7a7de395b6
karlof002 <57101995+karlof002@users.noreply.github.com>
2021-05-24 21:46:33 +0200

    Add files via upload

---
+++ app/public/scripts/end.js
@@ -0,0 +1,26 @@
+const username = document.querySelector('#username')
+const saveScoreBtn = document.querySelector('#saveScoreBtn')
+const finalScore = document.querySelector('#finalScore')
+const mostRecentScore = localStorage.getItem('mostRecentScore')
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+const MAX_HIGH_SCORES = 10
+finalScore.innerText = mostRecentScore
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value
+})
+
+saveHighScore = e => {
+    e.preventDefault()
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    }
+    console.log(score);
+    highScores.push(score)
+    highScores.sort((a,b) => {
+        return b.score - a.score
+    })
+    highScores.splice(MAX_HIGH_SCORES)
+    localStorage.setItem('highScores', JSON.stringify(highScores))
+    window.location.assign('#')
+}
