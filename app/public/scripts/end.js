de1685658c81359fdc2fb8569482ad92a84e250d
karlof002 <57101995+karlof002@users.noreply.github.com>
2021-05-24 20:46:01 +0200

    Update end.js

--- app/public/scripts/end.js
+++ app/public/scripts/end.js
@@ -3,22 +3,24 @@ const saveScoreBtn = document.querySelector('#saveScoreBtn')
 const finalScore = document.querySelector('#finalScore')
 const mostRecentScore = localStorage.getItem('mostRecentScore')
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
-const MAX_HIGH_SCORES = 5
+const MAX_HIGH_SCORES = 10
 finalScore.innerText = mostRecentScore
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value
 })
+
 saveHighScore = e => {
     e.preventDefault()
     const score = {
         score: mostRecentScore,
         name: username.value
     }
+    console.log(score);
     highScores.push(score)
     highScores.sort((a,b) => {
         return b.score - a.score
     })
-    highScores.splice(5)
+    highScores.splice(MAX_HIGH_SCORES)
     localStorage.setItem('highScores', JSON.stringify(highScores))
     window.location.assign('#')
 }
