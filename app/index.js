9607ee7bdebfe03bc57c3671f7bc8f555654aeff
karlof002 <57101995+karlof002@users.noreply.github.com>
2021-05-31 22:34:28 +0200

    Add files via upload

---
+++ app/index.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const server = http.createServer(app);
+const path = require('path');
+const port = 4000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+    res.render('index')
+})
+
+app.get("/game", (req, res) => {
+    res.render('game')
+})
+
+app.get("/highscores", (req, res) => {
+    res.render('highscores')
+})
+
+const listener = server.listen(port, function () {
+    console.log("Your app is listening on port " + listener.address().port);
+})
